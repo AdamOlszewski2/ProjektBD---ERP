5edc8d5c82a696128cc98dcc445f36d6ca4c2eec
NataliaSkreczko <natalias8@o2.pl>
2019-11-23 22:26:49 +0100

    Pierwsze DDL

---
+++ create_salesdocument.sql
@@ -0,0 +1,24 @@
+USE [ProjektBD]
+GO
+
+/****** Object:  Table [dbo].[SALESDOCUMENTS]    Script Date: 18.11.2019 17:25:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SALESDOCUMENTS](
+	[DOCID] [int] IDENTITY(1,1) NOT NULL,
+	[DOCNUMBER] [nvarchar](50) NOT NULL,
+	[SECTIONID] [int] NOT NULL,
+	[CONTRACTORID] [int] NOT NULL,
+	[USERID] [int] NOT NULL,
+	[MODDATE] [datetime] NOT NULL,
+	[CREATEDATE] [datetime] NOT NULL,
+	[INVOICEDATE] [datetime] NULL,
+	[GROSSSUM] [money] NOT NULL,
+	[NETSUM] [money] NOT NULL
+) ON [PRIMARY]
+GO
+
