9a30274872c1f770559104843c36dcea28a7d895
vasudanin <adam.a.olszewski@student.put.poznan.pl>
2019-11-25 11:17:28 +0100

    Poprawa tabel

--- 12_create_salesdocument.sql
+++ 12_create_salesdocument.sql
@@ -1,31 +1,3 @@
-<<<<<<< HEAD:12_create_salesdocument.sql
-USE [ProjektBD]
-GO
-
-/****** Object:  Table [dbo].[SALESDOCUMENTS]    Script Date: 18.11.2019 17:25:53 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[SALESDOCUMENTS](
-	[DOCID] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	[DOCNUMBER] [nvarchar](50) NOT NULL,
-	[SECTIONID] [int] NOT NULL,
-	[CONTRACTORID] [int] NOT NULL,
-	[USERID] [int] NOT NULL,
-	[MODDATE] [datetime] NOT NULL,
-	[CREATEDATE] [datetime] NOT NULL,
-	[INVOICEDATE] [datetime] NULL,
-	[GROSSSUM] [money] NOT NULL,
-	[NETSUM] [money] NOT NULL,
-	FOREIGN KEY(USERID) REFERENCES USERS(USERID),
-	CONSTRAINT NETSUM CHECK ( NETSUM >0),
-	CONSTRAINT EMAIL CHECK ( dbo.IsValidEmail('EMAIL') = 1),
-	CONSTRAINT NIP CHECK ( dbo.IsValidNip('NIP') = 1)
-) ON [PRIMARY]
-GO
 
 =======
 USE [ProjektBD]
@@ -56,4 +28,3 @@ CREATE TABLE [dbo].[SALESDOCUMENTS](
 ) ON [PRIMARY]
 GO
 
->>>>>>> 397c9e0a6ce92685920833ff9346dcba0afc045e:create_salesdocument.sql
