174c80990af495497359afd3898488e8c133cafc
vasudanin <adam.a.olszewski@student.put.poznan.pl>
2019-11-25 11:08:59 +0100

    Poprawa tabel

--- 3_create_function_isvalidnip.sql
+++ 3_create_function_isvalidnip.sql
@@ -8,17 +8,24 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE FUNCTION [dbo].[IsValidEmail](@EMAIL varchar(100))
-
-RETURNS bit as
-BEGIN     
-  DECLARE @bitRetVal as Bit
-  IF (@EMAIL <> '' AND @EMAIL NOT LIKE '_%@__%.__%')
-     SET @bitRetVal = 0  -- Invalid
-  ELSE 
-    SET @bitRetVal = 1   -- Valid
-  RETURN @bitRetVal
-END 
-GO
-
-
+CREATE FUNCTION IsValidNip
+(
+  @nip nvarchar(15)
+)
+RETURNS bit
+AS
+BEGIN
+  SELECT @nip = REPLACE(@nip,'-','')
+  IF ISNUMERIC(@nip) = 0
+	RETURN 0
+  DECLARE
+	@weights AS TABLE
+	(
+	  Position tinyint IDENTITY(1,1) NOT NULL,
+	  Weight tinyint NOT NULL
+	)
+  INSERT INTO @weights VALUES (6), (5), (7), (2), (3), (4), (5), (6), (7)
+  IF SUBSTRING(@nip, 10, 1) = (SELECT SUM(CONVERT(TINYINT, SUBSTRING(@nip, Position, 1)) * Weight) % 11 FROM @weights)
+	RETURN 1
+  RETURN 0
+END
