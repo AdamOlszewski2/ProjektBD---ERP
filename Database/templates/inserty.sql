f356185f9bec21566a3313af1e4b5a77e427b245
vasudanin <adam.a.olszewski@student.put.poznan.pl>
2019-12-15 11:57:08 +0100

    poprawa sekwencji

--- Database/templates/inserty.sql
+++ Database/templates/inserty.sql
@@ -6,8 +6,8 @@ insert into VATRATE values (0.23);
 insert into users values ('aa','aa','aa', cast('aaa' as VARBINARY(MAX)), 1);
 insert into contractor values ('adam1', 'poznan', 'aa@aa.pl', 1110523716, 1234);
 insert into product values (1234,'aaa'	,1	,2.54	,1);
-insert into saledocument values(NEXT VALUE FOR SALEDOCNUBMERSEQ,1,GETDATE(),GETDATE(),GETDATE(), 100,150);
-insert into invoicedocument values(1,1,1,GETDATE(),GETDATE(),GETDATE(), 10,15);
+insert into saledocument values(NEXT VALUE FOR [SALEDOCNUBMERSEQ],1,1,GETDATE(),GETDATE(),GETDATE(), 100,150);
+insert into invoicedocument values(NEXT VALUE FOR [INVOICEDOCNUBMERSEQ],1,1,GETDATE(),GETDATE(),GETDATE(), 10,15);
 
 INSERT INTO [dbo].[SALEDOCUMENT]
            ([DOCUMENTNUMBER]
@@ -41,11 +41,5 @@ update [SALEDOCUMENTPOSITION] set quantity = 1 where [SALEDOCUMENTPOSITIONID] = 
 
 delete from [SALEDOCUMENTPOSITION];
 
-<<<<<<< HEAD
-ALTER TABLE DBO.CONTRACTOR
-ADD CONSTRAINT UQ_NIP UNIQUE (NIP); 
-=======
 EXEC ALL_USERS_DOCUMENTS @FirstName = 'aa', @LastName = 'aa', @DocType = 4;
 
-alter table INVOICEDOCUMENTPOSITION add constraint FK__INVOICEDO__DOCUM__6C190EBB FOREIGN KEY(DocumentID) references INVOICEDOCUMENT(DocumentID);
->>>>>>> c08833199a94e9c4a315234c35ec70b06c3feea4
